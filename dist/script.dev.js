84cee58d5574c3be7df6f5d0f784e353cdde6915
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-02-21 16:59:54 +0000

    Trying to use index in my function. May be out of scope?

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -5,13 +5,15 @@ var tokenSpace = document.getElementsByClassName("playing-grid__token-space"); /
 
 var onGridSquareClick = function onGridSquareClick(event) {
   console.log("This function works");
-  event.target.classList.add("player-one");
+  event.target.classList.add("player-one"); // console.log(index);
+  // event.target.innerHTML = index;
 }; // Logic
 
 
 for (var index = 0; index < tokenSpace.length; index++) {
   tokenSpace[index].addEventListener("click", onGridSquareClick);
-} // Only grid squares with the class of 'allowed' are allowed to be clicked. Create an alert if not
+} // IS THERE A WAY TO USE/REFERENCE THIS INDEX IN THE onGridSquareClick FUNCTION???
+// Only grid squares with the class of 'allowed' are allowed to be clicked. Create an alert if not
 // After each allowed turn, the grid square is given class of player-one or player-two and the turn changes between player 1 and player 2
 // When an allowed grid square is clicked, the 'allowed' class is removed from that square and a new class of 'allowed' is added to the square above. Can do this by subtracting 7 from the value of the square
 // Figure out how determine winner!
--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const tokenSpace = document.getElementsByClassName("playing-grid__token-space");
 
 
 // Functions
-const onGridSquareClick = event => {
+const onGridSquareClick = (event) => {
   console.log("This function works");
   event.target.classList.add("player-one");
+  // console.log(index);
+  // event.target.innerHTML = index;
 }
 
 
@@ -14,6 +16,7 @@ const onGridSquareClick = event => {
 for (let index = 0; index < tokenSpace.length; index++) {
   tokenSpace[index].addEventListener("click", onGridSquareClick);
 }
+// IS THERE A WAY TO USE/REFERENCE THIS INDEX IN THE onGridSquareClick FUNCTION???
 
 
 // Only grid squares with the class of 'allowed' are allowed to be clicked. Create an alert if not
