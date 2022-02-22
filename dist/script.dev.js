23e5892594a356afbdfccfef0a083ec580cc4de2
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-02-22 14:50:38 +0000

    Used an anonymous function to allow the index parameter from the listener function to be used in my onGridSquareClick function

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,19 +1,34 @@
 "use strict";
 
 // Variables
-var tokenSpace = document.getElementsByClassName("playing-grid__token-space"); // Functions
+var tokenSpace = document.getElementsByClassName("playing-grid__token-space"); // // Functions
 
-var onGridSquareClick = function onGridSquareClick(event) {
-  console.log("This function works");
-  event.target.classList.add("player-one"); // console.log(index);
-  // event.target.innerHTML = index;
+var onGridSquareClick = function onGridSquareClick(event, index) {
+  event.target.classList.add("player-one");
+  console.log(index);
+  event.target.innerHTML = index;
 }; // Logic
 
 
+var _loop = function _loop(index) {
+  tokenSpace[index].addEventListener("click", function () {
+    onGridSquareClick(event, index);
+  });
+};
+
 for (var index = 0; index < tokenSpace.length; index++) {
-  tokenSpace[index].addEventListener("click", onGridSquareClick);
+  _loop(index);
 } // IS THERE A WAY TO USE/REFERENCE THIS INDEX IN THE onGridSquareClick FUNCTION???
+// E.g.
+// for (let index = 0; index < tokenSpace.length; index++) {
+//   tokenSpace[index].onclick = (event) => {
+//     console.log("This function works");
+//     event.target.classList.add("player-one");
+//     console.log(index);
+//     event.target.innerHTML = index;
+//   };
+// };
 // Only grid squares with the class of 'allowed' are allowed to be clicked. Create an alert if not
 // After each allowed turn, the grid square is given class of player-one or player-two and the turn changes between player 1 and player 2
-// When an allowed grid square is clicked, the 'allowed' class is removed from that square and a new class of 'allowed' is added to the square above. Can do this by subtracting 7 from the value of the square
+// When an allowed grid square is clicked, the 'allowed' class is removed from that square and a new class of 'allowed' is added to the square above. Can do this by subtracting 7 from the index value of the square
 // Figure out how determine winner!
--- script.js
+++ script.js
@@ -3,20 +3,30 @@
 const tokenSpace = document.getElementsByClassName("playing-grid__token-space");
 
 
-// Functions
-const onGridSquareClick = (event) => {
-  console.log("This function works");
+// // Functions
+const onGridSquareClick = (event, index) => {
   event.target.classList.add("player-one");
-  // console.log(index);
-  // event.target.innerHTML = index;
+  console.log(index);
+  event.target.innerHTML = index;
 }
 
 
 // Logic
 for (let index = 0; index < tokenSpace.length; index++) {
-  tokenSpace[index].addEventListener("click", onGridSquareClick);
+  tokenSpace[index].addEventListener("click", function () {onGridSquareClick(event, index)});
 }
 // IS THERE A WAY TO USE/REFERENCE THIS INDEX IN THE onGridSquareClick FUNCTION???
+
+// E.g.
+// for (let index = 0; index < tokenSpace.length; index++) {
+//   tokenSpace[index].onclick = (event) => {
+//     console.log("This function works");
+//     event.target.classList.add("player-one");
+//     console.log(index);
+//     event.target.innerHTML = index;
+//   };
+
+// };
 
 
 // Only grid squares with the class of 'allowed' are allowed to be clicked. Create an alert if not
@@ -24,6 +34,6 @@ for (let index = 0; index < tokenSpace.length; index++) {
 
 // After each allowed turn, the grid square is given class of player-one or player-two and the turn changes between player 1 and player 2
 
-// When an allowed grid square is clicked, the 'allowed' class is removed from that square and a new class of 'allowed' is added to the square above. Can do this by subtracting 7 from the value of the square
+// When an allowed grid square is clicked, the 'allowed' class is removed from that square and a new class of 'allowed' is added to the square above. Can do this by subtracting 7 from the index value of the square
 
 // Figure out how determine winner!
