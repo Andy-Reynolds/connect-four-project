251c345f83b8d04fc6852c242b4032d1228d12d3
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-02-23 16:31:44 +0000

    Moved winningCombinationArrays to a separate file and exported from there

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,15 +1,12 @@
 "use strict";
 
+var _winningCombinations = require("/data/winning-combinations.js");
+
 // Variables
 var tokenSpace = document.getElementsByClassName("playing-grid__token-space");
 var whoIsCurrentPlayer = document.getElementById("current-player");
 var whoIsWinner = document.getElementById("winner");
-var currentPlayer = 1;
-var winningCombinationsArrays = [// Horizontal
-[0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [7, 8, 9, 10], [8, 9, 10, 11], [9, 10, 11, 12], [10, 11, 12, 13], [14, 15, 16, 17], [15, 16, 17, 18], [16, 17, 18, 19], [17, 18, 19, 20], [21, 22, 23, 24], [22, 23, 24, 25], [23, 24, 25, 26], [24, 25, 26, 27], [28, 29, 30, 31], [29, 30, 31, 32], [30, 31, 32, 33], [31, 32, 33, 34], [35, 36, 37, 38], [36, 37, 38, 39], [37, 38, 39, 40], [38, 39, 40, 41], // Vertical
-[0, 7, 14, 21], [7, 14, 21, 28], [14, 21, 28, 35], [1, 8, 15, 22], [8, 15, 22, 29], [15, 22, 39, 36], [2, 9, 16, 23], [9, 16, 23, 30], [16, 23, 30, 37], [3, 10, 17, 24], [10, 17, 24, 31], [17, 24, 31, 38], [4, 11, 18, 25], [11, 18, 25, 32], [18, 25, 32, 39], [5, 12, 19, 26], [12, 19, 26, 33], [19, 26, 33, 40], [6, 13, 20, 27], [13, 20, 27, 34], [20, 27, 34, 41], // Diagonal SE
-[0, 8, 16, 24], [7, 15, 23, 31], [14, 22, 30, 38], [1, 9, 17, 25], [8, 16, 24, 32], [15, 23, 31, 39], [2, 10, 18, 26], [9, 17, 25, 33], [16, 24, 32, 40], [3, 11, 19, 27], [10, 18, 26, 34], [17, 25, 33, 41], // Diagonal SW
-[6, 12, 18, 24], [13, 19, 25, 31], [20, 26, 32, 38], [5, 11, 17, 23], [12, 18, 24, 30], [19, 25, 31, 37], [4, 10, 16, 22], [11, 17, 23, 29], [18, 24, 30, 36], [3, 9, 15, 21], [10, 16, 22, 28], [17, 23, 29, 35]]; // // Functions
+var currentPlayer = 1; // // Functions
 
 var onGridSquareClick = function onGridSquareClick(event, index) {
   if (event.target.classList.contains("allowed")) {
@@ -40,12 +37,12 @@ var onGridSquareClick = function onGridSquareClick(event, index) {
 };
 
 var checkForWinner = function checkForWinner() {
-  for (var index = 0; index < winningCombinationsArrays.length; index++) {
+  for (var index = 0; index < _winningCombinations.winningCombinationsArrays.length; index++) {
     // console.log(winningCombinationsArrays[index][0]);
-    var tokenOne = tokenSpace[winningCombinationsArrays[index][0]];
-    var tokenTwo = tokenSpace[winningCombinationsArrays[index][1]];
-    var tokenThree = tokenSpace[winningCombinationsArrays[index][2]];
-    var tokenFour = tokenSpace[winningCombinationsArrays[index][3]];
+    var tokenOne = tokenSpace[_winningCombinations.winningCombinationsArrays[index][0]];
+    var tokenTwo = tokenSpace[_winningCombinations.winningCombinationsArrays[index][1]];
+    var tokenThree = tokenSpace[_winningCombinations.winningCombinationsArrays[index][2]];
+    var tokenFour = tokenSpace[_winningCombinations.winningCombinationsArrays[index][3]];
 
     if (tokenOne.classList.contains("player-one") && tokenTwo.classList.contains("player-one") && tokenThree.classList.contains("player-one") && tokenFour.classList.contains("player-one")) {
       whoIsCurrentPlayer.innerHTML = "";
